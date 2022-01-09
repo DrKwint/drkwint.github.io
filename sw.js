fcff916b1d172a62246c304fceffb2de35d55d87
Eleanor Quint <DrKwint@gmail.com>
2022-01-09 11:49:08 -0600

    blog post update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220104.2309';
+const cacheName = 'chirpy-20220109.1148';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
